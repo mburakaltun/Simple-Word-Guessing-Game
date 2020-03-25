9f4c33c82c1b7f0285622425abe7493349d78f41
M. Burak Altun <mburakltun@gmail.com>
2020-03-26 01:43:35 +0300

    commit

---
+++ Game.swift
@@ -0,0 +1,34 @@
+//
+//  Game.swift
+//  ApplePie
+//
+//  Created by MBurak Altun on 26.03.2020.
+//  Copyright © 2020 MBurak Altun. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
+    
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
+}
